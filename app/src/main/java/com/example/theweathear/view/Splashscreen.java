8d5f8b53ad27f54dc042c67cff5ae2026c5add68
nguyenbaan26112k <65660392+nguyenbaan26112k@users.noreply.github.com>
2022-06-15 06:07:26 +0700

    update glide

--- app/src/main/java/com/example/theweathear/view/Splashscreen.java
+++ app/src/main/java/com/example/theweathear/view/Splashscreen.java
@@ -1,19 +1,26 @@
 package com.example.theweathear.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.bumptech.glide.Glide;
 import com.example.theweathear.R;
+import com.example.theweathear.databinding.ActivitySplashscreenBinding;
+import com.squareup.picasso.Picasso;
 
 public class Splashscreen extends AppCompatActivity {
+    ActivitySplashscreenBinding binding;
     public static int SPLASH_SCREEN = 5000;//thời gian splashscreen kết thúc
+    String urlGif = "https://cdn.dribbble.com/users/1353252/screenshots/7430583/media/f456446ffc1c9a1608b94d6d136dbc0d.gif";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splashscreen);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_splashscreen);
+        loadImg(urlGif);
         getTimeActivity();
     }
     private void getTimeActivity(){
@@ -25,6 +32,9 @@ public class Splashscreen extends AppCompatActivity {
                 finish();
             }
         },SPLASH_SCREEN);
+    }
+    void loadImg(String urlGif){
+        Glide.with(this).load(urlGif).into(binding.gif);
     }
 
 }
